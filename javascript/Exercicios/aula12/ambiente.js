29ac534a69cd5a2aec16074a7f366d556511d1f2
Vinicius Hansel <hanelvinicius@gmail.com>
2022-03-03 00:30:45 -0300

    js

---
+++ javascript/Exercicios/aula12/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5,1,4,6]
+
+num.sort()
+num.push(10)
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(num[0])
+let pos=num.indexOf(4)
+console.log(`${pos}`)
---
+++ javascript/Exercicios/aula12/vetornatela.js
@@ -0,0 +1,10 @@
+let num=[1,2,3,4,5,6,7,8]
+/*for(let pos=0;pos<num.length;pos++)
+{
+    console.log(`${num[pos]}\n`)
+}
+*/
+for (let pos in num)
+{
+    console.log(`${num[pos]}\n`)
+}
