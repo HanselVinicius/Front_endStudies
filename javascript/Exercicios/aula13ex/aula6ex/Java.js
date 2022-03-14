7119e08bac5a8f29cca3f9e5069842ffb0ecb5f9
Vinicius Hansel <hanelvinicius@gmail.com>
2022-03-14 17:12:41 -0300

    update

--- javascript/Exercicios/aula13ex/aula6ex/Java.js
+++ javascript/Exercicios/aula13ex/aula6ex/Java.js
@@ -29,10 +29,49 @@ function inlista(n,lista)
 function adciona(){
     if(isnumero(num.value)&& !inlista(num.value, valores))
     { 
-        window.alert('Tudo ok')
-
-    }else
-    {
+        valores.push(Number(num.value))
+        let item =document.createElement('option')
+        item.text=`Valor ${num.value} adcionado`
+        lista.appendChild(item)
+        res.innerHTML=''     
+    }
+        else{
         window.alert('Valor inválido ou já está na lista.')
     }
+    num.value=''
+    num.focus()
+}
+function Finalizar()
+{
+    if(valores.length==0)
+    {
+        window.alert('adcione valores antes de finalizar')
+    }else
+    {
+        let tot=valores.length
+        let maior=valores[0]
+        let menor=valores[0]
+        let soma=0
+        let media=0
+        for(let pos in valores)
+        {
+            soma+=valores[pos]
+            if(valores[pos]>maior)
+            {
+                maior=valores[pos]
+            }
+            else if(valores[pos]<menor){
+                menor=valores[pos]
+            }
+        }
+        media=soma/tot
+        res.innerHTML=''
+        res.innerHTML+=`<p>ao todo temos ${tot} numeros cadastrados</p>`
+        res.innerHTML+=`<p>O maior valor informado foi ${maior}<p/>`
+        res.innerHTML+=`<p>O menor valor informado foi ${menor}<p/>`
+        res.innerHTML+=`<p>A soma dos valores foi ${soma}<p/>`
+        res.innerHTML+=`<p>A media dos valores foi ${media}<p/>`
+
+    }
+
 }
